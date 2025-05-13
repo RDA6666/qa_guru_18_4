ec921b71f2d420bf3439d2f91f22b8144fc35ec1
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-05-13 16:04:03 +0300

    created build.gradle

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static jdk.internal.misc.ThreadFlock.open;
 
 
 public class SelenideRepositorySearch {
