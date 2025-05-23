4e7c041e95094a16d34a5fcc2201d2182d00cc63
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-05-23 19:41:19 +0300

    updated again checkIfJunit5IsThereTest

--- src/test/java/github/SoftAssertionsSearch.java
+++ src/test/java/github/SoftAssertionsSearch.java
@@ -1,22 +1,42 @@
 package github;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class SoftAssertionsSearch {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
     @Test
     void checkIfJnit5IsThere() {
         //  Откройте страницу Selenide в Github
-        open("https://github.com/selenide/selenide");
-       
+        open("/selenide/selenide");
+
         // - Перейдите в раздел Wiki проекта
-        //
+        $("#wiki-tab").click();
+
         // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-        //
+        $(".markdown-body").shouldHave(text("Soft assertions"));
+        $(byTagAndText("a", "Soft assertions")).shouldBe(visible);
+
         // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $(byTagAndText("a", "Soft assertions")).click();
+
+
+        sleep(5000);
 
     }
 }
