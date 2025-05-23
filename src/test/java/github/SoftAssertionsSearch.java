3e226865ce4e48d6f433d005953ca830bc639f0a
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-05-23 17:42:33 +0300

    created checkIfJunit5IsThereTest

---
+++ src/test/java/github/SoftAssertionsSearch.java
@@ -0,0 +1,18 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+public class SoftAssertionsSearch {
+
+    @Test
+    void checkIfJnit5IsThere() {
+        //  Откройте страницу Selenide в Github
+        //
+        // - Перейдите в раздел Wiki проекта
+        //
+        // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        //
+        // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+
+    }
+}
