5bcb4281a92af615f24cd519277c2eae80fef45e
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-05-23 18:09:15 +0300

    updated stuff

--- src/test/java/github/SoftAssertionsSearch.java
+++ src/test/java/github/SoftAssertionsSearch.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
+
 public class SoftAssertionsSearch {
 
     @Test
     void checkIfJnit5IsThere() {
         //  Откройте страницу Selenide в Github
-        open("https://github.com/");
-        open("/selenide/selenide");
-
+        open("https://github.com/selenide/selenide");
+       
         // - Перейдите в раздел Wiki проекта
         //
         // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
