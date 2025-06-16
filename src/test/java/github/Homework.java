78c2c0d532769055fbf32cf9443971ba1e31e86c
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-06-16 22:43:05 +0300

    updated Homework.java

--- src/test/java/github/Homework.java
+++ src/test/java/github/Homework.java
@@ -1,19 +1,18 @@
 package github;
 
-import com.codeborne.selenide.BaseElementsCollection;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Condition.text;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Homework extends TestBase {
 
     @Test
-    <SELF extends BaseElementsCollection<T, SELF>>
-    void successfulRegistrationTest() {
-        open("/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
+       void theHoverTest() {
+        // На главной странице GitHub выберите: Меню -> Solutions -> Enterprize (с помощью команды hover для Solutions)
+        open(baseUrl);
+        $(byTagAndText("button", "Solutions")).hover();
+
     }
 }
